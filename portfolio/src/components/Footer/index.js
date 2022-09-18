8ac1a21791603cb53ca603df519f14f97cad880d
Adam Smith <smith.adam937@gmail.com>
2022-09-18 17:11:05 -0500

    sticky footer created

--- portfolio/src/components/Footer/index.js
+++ portfolio/src/components/Footer/index.js
@@ -5,14 +5,14 @@ function Footer() {
     <footer>
 
       <ul className="iconz">
-        <li className="marginSpace-2">
+        <li>
           Copyright &copy; 2022
         </li>
         <li>
           < a href="https://https://www.linkedin.com/in/adam-smith-b41ab2250/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn"/></a>
         </li>
-        <li className="test">
-          <a href="https://github.com/smith-utxo"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="iconz"/></a>
+        <li>
+          <a href="https://github.com/smith-utxo"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github"/></a>
         </li>
       </ul>
 
--- portfolio/src/components/Header/index.js
+++ portfolio/src/components/Header/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Header(props) {
   return (
-    <div>
-      <h2>
+    <div >
+      <h2 className="PortfolioTitle">
         < a href="/">Adam Tyler</a>
       </h2>
       {props.children}
