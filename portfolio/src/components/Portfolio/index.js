46b5005c9e6a3cddc2f0a88ffcbd976c3a434d22
Adam Smith <smith.adam937@gmail.com>
2022-09-18 19:28:41 -0500

    Resume component Complete

--- portfolio/src/components/Portfolio/index.js
+++ portfolio/src/components/Portfolio/index.js
@@ -1,10 +1,29 @@
-import React from 'react'; 
+import React from 'react';
 
-function Portfolio() {
+
+const Portfolio = () => {
+
   return (
-    <section>
-      <h1>Name</h1>
-      <p>Description</p>
+    <section className="flexPortfolio">
+      <div>
+        < a href="https://group-project-dos.herokuapp.com//"><img src={require(`../../assets/CoverGallery/01_SudsOnSale.PNG`)} alt={`Suds on Sale`}/></a>
+      </div>
+      <div>
+        < a href="https://smith-utxo.github.io/Weather-Dashboard//"><img src={require(`../../assets/CoverGallery/02_WeatherDashboard.PNG`)} alt={`Weather Dashboard`}/></a>
+      </div>
+      <div>
+        < a href="https://smith-utxo.github.io/Weather-Dashboard//"><img src={require(`../../assets/CoverGallery/03_Obscura.png`)} alt={`Obscura`}/></a>
+      </div>
+      <div>
+        < a href="https://smith-utxo.github.io/run-buddy/"><img src={require(`../../assets/CoverGallery/04_Run-buddy.PNG`)} alt={`Run Buddy`}/></a>
+      </div>
+      <div>
+        < a href="https://smith-utxo.github.io/Work-Day-Scheduler/"><img src={require(`../../assets/CoverGallery/05_Workday Scheduler.png`)} alt={`Workday Scheduler`}/></a>
+      </div>
+      <div>
+      < a href="https://smith-utxo.github.io/Code-Quiz/"><img src={require(`../../assets/CoverGallery/06_Code_Quiz.png`)} alt={`Code Quiz`}/></a>
+      </div>
+
     </section>
   )
 }
--- portfolio/src/components/Resume/index.js
+++ portfolio/src/components/Resume/index.js
@@ -2,8 +2,25 @@ import React from 'react';
 
 
 function Resume() {
-  return(
-    <section> This is where my resume will go! </section>
+  return (
+    <section>
+
+      <br></br>
+
+      <h3>Overview of experience </h3>
+      <ul className="flex-column">
+        <li>=> MERN Stack: MongoDB, Express, React, and Node</li>
+        <li>=> HTML, CSS, & Git</li>
+        <li>=> JavaScript ES5 & ES6</li>
+        <li>=> API's: 3rd Party, Web, & Server Side</li>
+        <li>=> SQL and Sequelize </li>
+        <li>=> ORM, MVC and PWA</li>
+      </ul>
+
+      <a href="https://drive.google.com/file/d/1qruRXsXeEDEto3AN-SpLRQOI6BLj78uR/view?usp=sharing" className="brightpink" target="_blank" rel="noreferrer">Download my resume.</a>
+
+
+    </section>
   )
 }
 
