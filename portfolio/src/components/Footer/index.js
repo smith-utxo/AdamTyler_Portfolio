5878f18d7c3fd9f6078814b1f6fbde10c7377c90
Adam Smith <smith.adam937@gmail.com>
2022-09-18 19:36:49 -0500

    Linkedin Link in Footer Fix

--- portfolio/src/components/Footer/index.js
+++ portfolio/src/components/Footer/index.js
@@ -9,7 +9,7 @@ function Footer() {
           Copyright &copy; 2022
         </li>
         <li>
-          < a href="https://https://www.linkedin.com/in/adam-smith-b41ab2250/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn"/></a>
+          < a href="https://www.linkedin.com/in/adam-smith-b41ab2250/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn"/></a>
         </li>
         <li>
           <a href="https://github.com/smith-utxo"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github"/></a>
