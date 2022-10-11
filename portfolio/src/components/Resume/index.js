7338419d603ffc4bf746356715aa6a30232363d1
Adam Smith <smith.adam937@gmail.com>
2022-10-11 13:37:40 -0500

    resume link updated

--- portfolio/src/components/Resume/index.js
+++ portfolio/src/components/Resume/index.js
@@ -9,15 +9,15 @@ function Resume() {
 
       <h3>Overview of experience </h3>
       <ul className="flex-column">
-        <li>=> MERN Stack: MongoDB, Express, React, and Node</li>
-        <li>=> HTML, CSS, & Git</li>
-        <li>=> JavaScript ES5 & ES6</li>
-        <li>=> API's: 3rd Party, Web, & Server Side</li>
-        <li>=> SQL and Sequelize </li>
-        <li>=> ORM, MVC and PWA</li>
+        <li> MERN Stack: MongoDB, Express, React, and Node</li>
+        <li> HTML, CSS, & Git</li>
+        <li> JavaScript ES5 & ES6</li>
+        <li> API's: 3rd Party, Web, & Server Side</li>
+        <li> SQL and Sequelize </li>
+        <li> ORM, MVC and PWA</li>
       </ul>
 
-      <a href="https://drive.google.com/file/d/1qruRXsXeEDEto3AN-SpLRQOI6BLj78uR/view?usp=sharing" className="brightpink" target="_blank" rel="noreferrer">Download My Resume Here</a>
+      <a href="https://docs.google.com/document/d/1A481Q91CVc_MyX6eQhsCiLnLdHz92i_wUd5I7H1yESg/edit?usp=sharing" className="brightpink" target="_blank" rel="noreferrer">Download My Resume Here</a>
 
 
     </section>
