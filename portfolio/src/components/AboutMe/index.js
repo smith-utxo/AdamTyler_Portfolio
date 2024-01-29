2e68ddaf13d584b98bb00125e94b8e2b42e52bde
Adam Smith <smith.adam937@gmail.com>
2024-01-28 21:04:32 -0600

    Update AboutMe component with revised description

--- portfolio/src/components/AboutMe/index.js
+++ portfolio/src/components/AboutMe/index.js
@@ -6,15 +6,28 @@ function AboutMe() {
   return (
     <section className="flex-row">
       <div>
-        <img src={headShot} className="roundBorder" style={{ width: "50%" }} alt="cover" />
+        <img
+          src={headShot}
+          className="roundBorder"
+          style={{ width: "50%" }}
+          alt="cover"
+        />
       </div>
       <div className="Margin-10">
         <h4>
-        Web developer trained in agile methodology that leverages quality improvement background to deliver reliable and intuitive applications. Recently obtained a certificate in full stack development from the University of Minnesota with a focus on the MERN stack. I consider myself a lifelong learner with a strong desire to continue improving my abilities and the projects I work on. I believe perfection is a fallacy and instead strive for continuous improvement. I enjoy building accessible and responsive websites that engage every user. I'm well positioned to leverage my diverse background and skill set to create exceptional user experiences.
+          Jr. Software developer for a transportation
+          management company with exposure to both backend and frontend
+          development. Full stack web development certificate from the
+          University of Minnesota and a bachelor of science in marketing. Emphasis on effective communication and
+          continuous learning. I consider myself a lifelong learner with a strong desire to
+          continue improving my abilities and the projects I work on. I enjoy
+          building accessible and responsive websites that engage every user.
+          I'm well positioned to leverage my diverse background and skill set to
+          create exceptional user experiences.
         </h4>
       </div>
     </section>
-  )
+  );
 }
 
 export default AboutMe; 
